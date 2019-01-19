3ec04ede5464c5425c4fac25ba55cdce1d48db9a
hkerkhoff0563@student.phoenixunion.org <streetfighter@localhost.localdomain>
2019-01-19 10:54:16 -0700

    Brandons code: Get robot to drive

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,34 +5,115 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-//hello world
 package frc.robot;
 
-import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PWMVictorSPX;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+    Joystick stick; 
+    Joystick stick1;
+    DifferentialDrive myDrive;
+    private double m_speedLimiter;
+    private int speedLimiterAxis;
+    Double leftm;
+    Double rightm; 
 
-public class Robot extends IterativeRobot {
-
-  private DifferentialDrive m_myRobot;
-  private Joystick m_leftStick;
-  private Joystick m_rightStick;
-
+  /**
+   * This function is run when the robot is first started up and should be
+   * used for any initialization code.
+   */
   @Override
   public void robotInit() {
-      m_myRobot=new DifferentialDrive(new PWMVictorSPX(0), new PWMVictorSPX(1));
-      m_leftStick=new Joystick(0);
-      m_rightStick=new Joystick(1);
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+    SpeedControllerGroup rightSpeedGroup = new SpeedControllerGroup( new PWMVictorSPX(0), new PWMVictorSPX(2));
+        SpeedControllerGroup leftSpeedGroup = new SpeedControllerGroup(new PWMVictorSPX(1), new PWMVictorSPX(3)); 
+        stick = new Joystick(0); 
+
+        myDrive = new DifferentialDrive(rightSpeedGroup, leftSpeedGroup);
+        m_speedLimiter = stick.getRawAxis(speedLimiterAxis); 
+        leftm= -stick.getRawAxis(1)/(2-m_speedLimiter); 
+        rightm= -stick.getRawAxis(5)/(2-m_speedLimiter); 
   }
 
+  /**
+   * This function is called every robot packet, no matter the mode. Use
+   * this for items like diagnostics that you want ran during disabled,
+   * autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before
+   * LiveWindow and SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+  }
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select
+   * between different autonomous modes using the dashboard. The sendable
+   * chooser code works with the Java SmartDashboard. If you prefer the
+   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+   * getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to
+   * the switch structure below with additional strings. If using the
+   * SendableChooser make sure to add them to the chooser code above as well.
+   */
+  @Override
   public void autonomousInit() {
-
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
   }
 
+  /**
+   * This function is called periodically during autonomous.
+   */
   @Override
   public void autonomousPeriodic() {
-    m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY());
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        break;
+    }
+  }
+
+  /**
+   * This function is called periodically during operator control.
+   */
+  @Override
+  public void teleopPeriodic() {
+
+    myDrive.tankDrive(stick.getY(), stick1.getY());
+  }
+
+  /**
+   * This function is called periodically during test mode.
+   */
+  @Override
+  public void testPeriodic() {
   }
 }

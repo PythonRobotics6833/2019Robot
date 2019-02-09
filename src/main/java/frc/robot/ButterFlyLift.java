0e5777a3bcab14dfef2695d965168a2f54fe69b1
hkerkhoff0563@student.phoenixunion.org <streetfighter@localhost.localdomain>
2019-02-09 14:54:04 -0700

    Added Analog move method and part of DigitalMove method to butterflylift

--- src/main/java/frc/robot/ButterFlyLift.java
+++ src/main/java/frc/robot/ButterFlyLift.java
@@ -7,8 +7,11 @@
 
 package frc.robot;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.VictorSP;
 
 /**
  * Add your docs here.
@@ -20,7 +23,10 @@ public class ButterFlyLift
      double speedBot;
      double speedMid;
      ControlMode Up;
-     Joystick Player2;  
+     Joystick Player2; 
+     VictorSP pivotPoint; 
+
+    boolean CargoHeight;
     public ButterFlyLift(int Talon1, int Talon2){
         Bottom = new TalonSRX(Talon1);
         Middle = new TalonSRX(Talon2);
@@ -28,10 +34,34 @@ public class ButterFlyLift
         speedBot = 0.1;
         speedMid = 0.1; 
                                                 }
+    public ButterFlyLift(int Victor, Joystick Player2)
+    {
+        pivotPoint= new VictorSP(Victor);
+        this.Player2=Player2;
+        CargoHeight=false;
+    }
 
     public void speed (double speedBot, double speedMid){
         Bottom.set(Up, speedBot);
         Middle.set(Up, speedMid);
 
-                                                        }
     }
+    //Call this method in order to manually move th arm based on the joystick
+    public void analogMove()
+    {
+        pivotPoint.setSpeed(Player2.getRawAxis(1)/4);
+    }
+    public void digitalMove()
+    {   
+        double fineHeight= Player2.getRawAxis(2);
+        //
+        int heightLevel;
+        if(Player2.getRawAxis(2)>=0.5)
+        {
+            CargoHeight=true;
+        }
+        else if(Player2.get)
+    }
+
+}
+

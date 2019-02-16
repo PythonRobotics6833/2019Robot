4ca2a05a50fcc75640ce2c5b21215ba014e18498
hkerkhoff0563@student.phoenixunion.org <streetfighter@localhost.localdomain>
2019-02-15 20:30:03 -0700

    Finished non calibrated digital move

--- src/main/java/frc/robot/ButterFlyLift.java
+++ src/main/java/frc/robot/ButterFlyLift.java
@@ -7,9 +7,9 @@
 
 package frc.robot;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.DigitalInput;
 /**
  * Add your docs here.
@@ -19,7 +19,6 @@ public class ButterFlyLift
      TalonSRX Bottom;  
      TalonSRX Middle;
      double armSpeed=0.5;
-     ControlMode Up;
      Joystick Player2; 
      VictorSP armMotor; 
 
@@ -34,23 +33,24 @@ public class ButterFlyLift
     //When it is set to 2 it is at the heightest level of the robot
     //When it is set to -1 is a general Error
     int heightLevel;
-    //this is a placeholder until we install and test the encoder
+    //This is the variable that the encoder value is stored in
     double encoder;
     //this is added and subtracted to the encoder position values to give it a range in which it will stop instead of an exact value
     double deadZone;
-    //Digital input for a switch at the bottom of the arm
-    DigitalInput LowPoint;
     //These two varaibles will be filled with the ecoder posision that is required to be at a certain height.
+    Double lowLevelEncoderV;
     Double midLevelEncoderV;
     Double highLevelEncoderV;
-    //This is the value added to the encoder position so the arm can find its position
+    //This is the value added to the encoder position so the arm can find its position and is constant
     Double cargoHeightEncoderV;
 
+    AnalogPotentiometer liftEncoder;
     //Constructor for Talons
     public ButterFlyLift(int Talon1, int Talon2)
     {
         Bottom = new TalonSRX(Talon1);
         Middle = new TalonSRX(Talon2);
+        liftEncoder=new AnalogPotentiometer(0,10,0);
         
     }
     public ButterFlyLift(int Victor, Joystick Player2)
@@ -59,14 +59,10 @@ public class ButterFlyLift
         this.Player2=Player2;
         CargoHeight=false;
 
-        LowPoint= new DigitalInput(0);
+        liftEncoder=new AnalogPotentiometer(0,10,0);
     }
 
-    public void speed (double speedBot, double speedMid){
-        Bottom.set(Up, speedBot);
-        Middle.set(Up, speedMid);
-
-    }
+   
     //Call this method in order to manually move th arm based on the joystick
     public void analogMove()
     {
@@ -74,11 +70,16 @@ public class ButterFlyLift
     }
     //This uses the controller to set the arm to direct positions on the robot
     public void digitalMove()
-    {   
+    {      
+        //Get controller inputs and put it in variables
         double fineHeight= Player2.getRawAxis(2);
         lowLevelButton=Player2.getRawButtonPressed(1);
         midLevelButton=Player2.getRawButtonPressed(2);
         highLevelButton=Player2.getRawButtonPressed(3);
+
+        encoder=liftEncoder.get();
+        double offset;
+        
         if(fineHeight>=0.5)
         {
             CargoHeight=true;
@@ -86,6 +87,15 @@ public class ButterFlyLift
         else if(fineHeight<=-0.5)
         {
             CargoHeight=false;
+        }
+
+        if(CargoHeight)
+        {
+            offset=cargoHeightEncoderV;
+        }
+        else
+        {
+            offset=0;
         }
 
         if(lowLevelButton)
@@ -103,28 +113,28 @@ public class ButterFlyLift
 
         switch(heightLevel)
         {
-            case 0: setLevel(0, CargoHeight);
+            case 0: setLevel(0, offset);
             break;
 
-            case 1: setLevel(1, CargoHeight);
+            case 1: setLevel(1, offset);
             break;
 
-            case 2: setLevel(2, CargoHeight);
+            case 2: setLevel(2, offset);
             break;
 
-            default: setLevel(0, CargoHeight);
+            default: setLevel(0, offset);
             break;
         }
         
     }
     //This method is used to actuate the arm to its position by using the encoder as its guide
-    private void setLevel(int level, boolean heightlevel)
+    private void setLevel(int level, double offset)
     {
         if(level ==0)
         {
-            if(LowPoint.get())
+            if(encoder<(lowLevelEncoderV+deadZone+offset))
             {
-                return;
+                armMotor.setSpeed(0);
             }
             else{
                 armMotor.setSpeed(-armSpeed);;
@@ -132,11 +142,11 @@ public class ButterFlyLift
         }
         else if(level == 1)
         {
-            if(encoder<midLevelEncoderV-deadZone)
+            if(encoder<(midLevelEncoderV-deadZone+offset))
             {
                 armMotor.setSpeed(armSpeed);
             }
-            else if(encoder>midLevelEncoderV+deadZone)
+            else if(encoder>(midLevelEncoderV+deadZone+offset))
             {
                 armMotor.setSpeed(-armSpeed);
             }
@@ -147,12 +157,12 @@ public class ButterFlyLift
         }
         else if(level == 2)
         {
-            if(encoder<highLevelEncoderV-deadZone)
+            if(encoder<(highLevelEncoderV-deadZone+offset))
             {
                 armMotor.setSpeed(armSpeed);
             }
             //This should never happen but if something goes wrong with the gears this will force it to back off.
-            else if(encoder>highLevelEncoderV+deadZone)
+            else if(encoder>(highLevelEncoderV+deadZone+offset))
             {
                 armMotor.setSpeed(-armSpeed);
             }
